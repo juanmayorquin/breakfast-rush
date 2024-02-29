82a4211dd10d709a8a5f7707d72b89e3a7f7ae67
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2024-02-29 17:19:14 -0500

    Ahora sí, ya está

--- Assets/Scripts/ScoreGUI.cs
+++ Assets/Scripts/ScoreGUI.cs
@@ -7,28 +7,16 @@ public class ScoreGUI : MonoBehaviour
 {
 
     private TextMeshProUGUI scoreText;
-    [SerializeField] private GameObject newRecordText;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreText = GetComponent<TextMeshProUGUI>();
-        if (newRecordText != null)
-        {
-            newRecordText.SetActive(false);
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = Leaderboard.currentPlayerScore.ToString();
-        if (newRecordText != null)
-        {
-            if (Leaderboard.currentPlayerScore > Leaderboard.PlayerList[0].score)
-            {
-                newRecordText.SetActive(true);
-            }
-        }
     }
 }
